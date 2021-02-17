ba2b34476ecac17effc1b203e0fd09ed77b38502
hillham7 <78826177+hillham7@users.noreply.github.com>
2021-02-16 19:03:55 -0500

    Added the search engine, created the city search feature & deleted some old CSS styling

--- script.js
+++ script.js
@@ -32,8 +32,19 @@ function displayTemperature(response) {
     iconElement.setAttribute("alt", `response.data.weather[0].main`);
 }
 
-let apiKey = "9338a3569f57c5ead06164ec6559286c";
-let city = "New York";
-let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=imperial`;
+function search(city) {
+    let apiKey = "9338a3569f57c5ead06164ec6559286c";
+    let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=imperial`;
+    axios.get(apiUrl).then(displayTemperature);
+}
 
-axios.get(apiUrl).then(displayTemperature);
+function handleSubmit(event) {
+    event.preventDefault();
+    let cityInputElement = document.querySelector("#city-input");
+    search(cityInputElement.value);
+}
+
+search("New York");
+
+let form = document.querySelector("#search-form");
+form.addEventListener("submit", handleSubmit);
